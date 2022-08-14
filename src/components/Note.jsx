ba76d2ec05050eab33633e95a7eb3eed3a61b464
Rahul Jha <104366355+rahuljha4171@users.noreply.github.com>
2022-08-14 20:52:48 +0530

    modify till v2

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h2>This is Note Title</h2>
+      <p>This is note content</p>
+    </div>
+  );
+}
+
+export default Note;
